7cde711ee24cca4c1bfecb8d6f5f5a69bc1c6440
Devtonic <devtonic0@gmail.com>
2024-08-22 11:24:05 -0700

    refactored, all working and complete. except variable fees

---
+++ src/abi/erc20_abi.ts
@@ -0,0 +1,37 @@
+export const ERC20_ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        name: "",
+        type: "uint8",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+      },
+    ],
+    type: "function",
+  },
+];
